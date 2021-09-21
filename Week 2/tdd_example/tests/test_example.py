2128f21da8d14b9088dff50d70f53779c1ca8ca0
unknown <lizthompson07@gmail.com>
2021-09-21 17:11:01 -0300

    add tdd examples

---
+++ Week 2/tdd_example/tests/test_example.py
@@ -0,0 +1,10 @@
+# an example test
+'''
+To run use pytest, to get verbose use pytest -v
+'''
+
+def test_the_truth(): # A test function that does nothing
+    pass
+
+def test_basic_math(): # Test basic math
+    assert 2 + 2 == 4
